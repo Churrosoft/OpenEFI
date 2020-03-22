dba5d99b6c84ab34b13346ff0de6ea2c963d6e3b
Ramiro Bou <bouramiro@gmail.com>
2020-03-22 20:11:41 -0300

    Olvide commitear este archivo..

---
+++ src/serial/commands.h
@@ -0,0 +1,22 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define PROTOCOL_VERSION_1 1
+
+#define COMMAND_PING 1
+#define COMMAND_TMP 2
+#define COMMAND_RPM 3
+#define COMMAND_00V 4
+#define COMMAND_AVC 5
+#define COMMAND_INY 6
+#define COMMAND_LMB 7
+#define COMMAND_DBG 8
+// Enviado cuando ocurre un error. El error se especifica en el subcomando
+#define COMMAND_ERR 9
+#define ERROR_INVALID_PROTOCOL 1
+#define ERROR_INVALID_COMMAND 2
+#define COMMAND_WELCOME 10
+
+#define SUBCOMMAND_NONE 0
+
+#endif
