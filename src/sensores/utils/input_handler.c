8efcc3c8b4132019587d65f68b8f2544c5b45358
FDSoftware <dfrenoux@gmail.com>
2020-03-30 21:07:01 -0300

    fixes on ADC

--- src/sensores/utils/input_handler.c
+++ src/sensores/utils/input_handler.c
@@ -35,29 +35,25 @@ void input_setup(){
 	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO15);
 }
 
-static void adc_setup(void)
-{
-	int i;
+static void adc_setup(void){
 
 	rcc_periph_clock_enable(RCC_ADC1);
-
 	/* Make sure the ADC doesn't run during config. */
 	adc_power_off(ADC1);
-
 	/* We configure everything for one single conversion. */
 	adc_disable_scan_mode(ADC1);
 	adc_set_single_conversion_mode(ADC1);
 	adc_disable_external_trigger_regular(ADC1);
 	adc_set_right_aligned(ADC1);
-	/* We want to read the temperature sensor, so we have to enable it. */
+
+	/* SOLO PARA TESTEAR, agarramos el sensor de temperatura integrado y usamos eso como entrada analogica */
+	/* luego tendria que reemplazar esto por el canal analogico que vaya a usar*/
 	adc_enable_temperature_sensor();
 	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_28DOT5CYC);
-
 	adc_power_on(ADC1);
 
-	/* Wait for ADC starting up. */
-	for (i = 0; i < 800000; i++)    /* Wait a bit. */
-		__asm__("nop");
+	/* tiempo de espera del ADC, tanto tarda?, luego pasar a proceso asincrono de ultima*/
+	for (uint8_t i = 0; i < 800000; i++) __asm__("nop");
 
 	adc_reset_calibration(ADC1);
 	adc_calibrate(ADC1);
