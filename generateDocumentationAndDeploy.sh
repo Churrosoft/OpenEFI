7e7214ca4aada0d3c963fbc46acf5ceb1b25fa83
FDSoftware <dfrenoux@gmail.com>
2020-04-12 23:12:27 -0300

    doxygen test

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -71,10 +71,7 @@ echo "" > .nojekyll
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-cd ../../
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
-cd code_docs
-cd $GH_REPO_NAME
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
 # Only upload if Doxygen successfully created the documentation.
