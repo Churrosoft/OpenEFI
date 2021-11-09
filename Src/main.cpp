26f9363ac601e04ea142574643ca6edc92fa74ea
FDSoftware <dfrenoux@gmail.com>
2021-11-09 11:32:21 -0300

    clock issue?

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,33 +1,28 @@
 /* USER CODE BEGIN Header */
 /**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
-#define DEBUG
+/* Includes ------------------------------------------------------------------*/
 /* Includes ------------------------------------------------------------------*/
 extern "C"
 {
 #include "main.h"
-#include "adc.h"
-#include "can.h"
-#include "dma.h"
-#include "spi.h"
-#include "tim.h"
-#include "usb_device.h"
+// #include "spi.h"
 #include "gpio.h"
 #include "ll_spi.h"
 }
@@ -38,23 +33,16 @@ extern "C"
 #include <trace.h>
 #endif
 
-#ifdef TESTING
-#pragma GCC warning "TESTING ENABLED"
-#include <unity.h>
-#include "../test/cpwm_test.cpp"
-extern int run_tests(void);
-#endif
+#include "pmic/pmic.hpp"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "aliases/memory.hpp"
-#include "sensors/sensors.hpp"
-#include "variables.h"
-#include "pmic/pmic.hpp"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+bool MOTOR_ENABLE;
+bool SINC;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -67,14 +55,16 @@ extern int run_tests(void);
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-bool MOTOR_ENABLE;
-bool SINC;
+SPI_HandleTypeDef hspi2;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_SPI2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -98,13 +88,10 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-/* USER CODE BEGIN Init */
-#ifdef TESTING
-  trace_initialize();
-  printf("INIT_TESTING \n");
-  run_tests();
-  puts("END_TESTING \n");
-#endif
+
+  /* USER CODE BEGIN Init */
+
+  MOTOR_ENABLE = true;
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -116,109 +103,29 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-
-  // disable PMIC until setup
-  PMIC::init();
-  //HAL_SPI_MspInit(&hspi2);
-  // Initialize rest of peripherals
-  MX_DMA_Init();
-  MX_ADC1_Init();
-  MX_CAN1_Init();
   MX_SPI2_Init();
-  MX_TIM3_Init();
-  MX_TIM4_Init();
-  MX_TIM9_Init();
-  MX_ADC2_Init();
-  MX_TIM1_Init();
-
-  // MX_USB_DEVICE_Init();
-
   /* USER CODE BEGIN 2 */
-
-#if Alpha == 1
-  MOTOR_ENABLE = true;
-#else
-  MOTOR_ENABLE = can_turn_on();
-#endif
-  /* USER CODE END 2 */
-  HAL_GPIO_WritePin(MEMORY_CS_GPIO_Port, MEMORY_CS_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(AUX_CS_1_GPIO_Port, AUX_CS_1_Pin, GPIO_PIN_SET);
-  // memory::write_single(0xFFF, 0xDD);
 
+  HAL_Delay(100);
   PMIC::enable();
-  // PMIC::setup_spark();
-  HAL_Delay(100);
-/* 
-  memory::write_single(0xFFF, 0xDD);
-  memory::write_single(0xFFF, 0xDD);
-  memory::write_single(0xFFF, 0xDD);
-  memory::write_single(0xFFF, 0xDD); */
-
-  HAL_Delay(100);
-  uint8_t pData[] = {0xDF, 0xDF, 0xDF, 0xDF, 0xDF};
-  HAL_SPI_Transmit(&hspi2, pData, 4, HAL_MAX_DELAY - 1);
-  HAL_SPI_Transmit(&hspi2, pData, 4, HAL_MAX_DELAY - 1);
-
-  HAL_SPI_Transmit(&hspi2, pData, 4, HAL_MAX_DELAY - 1);
-
-  /*   for (uint8_t i = 0; i < 200; i++)
-  {
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
-  } */
-  HAL_Delay(100);
-
-  /* Infinite loop */
-
-  // habilitamos el pmic:
-  /*   HAL_GPIO_WritePin(INY1_GPIO_Port, INY1_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(ECN1_GPIO_Port, ECN1_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(PMIC_ENABLE_GPIO_Port, PMIC_ENABLE_Pin, GPIO_PIN_RESET);
-  HAL_Delay(100);
-
-
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_RESET);
-  spi_send_byte(0b01001011);
-  spi_send_byte(0b00111101);
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_SET);
-
-  // a seguro se lo levaron preso asi que:
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_RESET);
-  spi_read_byte();
-  spi_read_byte();
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_SET);
-
-  HAL_Delay(100);
-
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_RESET);
+  PMIC::demo_spark();
+  //HAL_Delay(100);
+  uint8_t pData[] = {0xAD, 0xDF, 0xDF, 0xDF};
+  /* USER CODE END 2 */
+  trace_printf("Init SPI \r\n");
   PMIC::dtc_check();
-  // 0x6A84 = 0110 1100 1000 0100
-  //0110 (DAC cmd)
-  //1100 (Max current = 19A )
-  //100 (overlap setting: 50% )
-  //00100 (Nominal current = 4A)
-
-  spi_send_byte(0b01101100);
-  spi_send_byte(0b10000100);
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_SET);
-
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_RESET);
-  spi_read_byte();
-  spi_read_byte();
-  HAL_GPIO_WritePin(PMIC_CS_GPIO_Port, PMIC_CS_Pin, GPIO_PIN_SET);
- */
-  volatile uint8_t data;
-  memory::write_single(0xFFF, 0xDD);
-  HAL_Delay(800);
-  data = memory::read_single(0xFF);
-
+  /*   spi_send_byte(0xFD);
+  HAL_SPI_Transmit(&hspi2, pData, 3, 1000); */
+  /* Infinite loop */
+  trace_printf("end SPI \r\n");
+  //HAL_Delay(100);
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
     /* USER CODE END WHILE */
-    //PMIC::demo_spark();
-    PMIC::dtc_check();
-    HAL_Delay(2000);
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -265,9 +172,145 @@ void SystemClock_Config(void)
   }
 }
 
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  //hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.DataSize = SPI_DATASIZE_16BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+/* void MX_GPIO_Init(void)
+{
+
+  // GPIO Ports Clock Enable
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+}
+ */
 /* USER CODE BEGIN 4 */
 
+void HAL_SPI_MspInit(SPI_HandleTypeDef *spiHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if (spiHandle->Instance == SPI2)
+  {
+    /* USER CODE BEGIN SPI2_MspInit 0 */
+
+    /* USER CODE END SPI2_MspInit 0 */
+    /* SPI2 clock enable */
+    __HAL_RCC_SPI2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**SPI2 GPIO Configuration
+    PB10     ------> SPI2_SCK
+    PB14     ------> SPI2_MISO
+    PB15     ------> SPI2_MOSI
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_14 | GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    //GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* USER CODE BEGIN SPI2_MspInit 1 */
+
+    /* USER CODE END SPI2_MspInit 1 */
+  }
+}
 /* USER CODE END 4 */
+
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
 
 /**
   * @brief  Period elapsed callback in non blocking mode
@@ -291,25 +334,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE END Callback 1 */
 }
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  HAL_GPIO_WritePin(MIL_OUT_GPIO_Port, MIL_OUT_Pin, GPIO_PIN_SET);
-  while (1)
-  {
-    HAL_GPIO_WritePin(CHK_OUT_GPIO_Port, CHK_OUT_Pin, GPIO_PIN_SET);
-    HAL_Delay(100);
-    HAL_GPIO_WritePin(CHK_OUT_GPIO_Port, CHK_OUT_Pin, GPIO_PIN_RESET);
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
 #ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
@@ -321,11 +345,10 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line
-     number,
+  /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  trace_printf("Wrong parameters value: file %s on line %d\r\n", file, line);
   /* USER CODE END 6 */
+  trace_printf("Wrong parameters value: file %s on line %d\r\n", file, line);
 }
 #endif /* USE_FULL_ASSERT */
 
