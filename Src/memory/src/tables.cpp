0a909d3f64115c642a2903f93961366cc7d65052
FDSoftware <dfrenoux@gmail.com>
2021-12-24 16:05:05 -0300

    FIX: change datasize to 32b on tables namespace, segfault goes brrr

--- Src/memory/src/tables.cpp
+++ Src/memory/src/tables.cpp
@@ -7,27 +7,34 @@ extern "C"
 using namespace std;
 #include "defines.h"
 
-int16_t tables::get_value(table_ref table, uint16_t x, uint16_t y)
+int32_t tables::get_value(table_ref table, uint16_t x, uint16_t y)
 {
     uint8_t data[3];
     uint32_t address = get_address(x, table.x_max, y, table.memory_address);
 
     W25qxx_ReadBytes(data, address, 2);
 
-    return ((int16_t)data[0] << 8) + data[1];
+    return (int32_t)(data[1] << 8) + (data[2] << 16) + (data[3] << 24) + data[0];
 }
 
 void tables::set_value(table_ref table, uint16_t x, uint16_t y, int16_t value)
 {
     uint32_t address = get_address(x, table.x_max, y, table.memory_address);
 
-    uint8_t data[2];
+    uint8_t data[4];
 
-    data[0] = (value >> 8) & 0xFF;
-    data[1] = value & 0xFF;
+    /*     data[0] = (value >> 8) & 0xFF;
+    data[1] = value & 0xFF; */
+
+    data[0] = (uint8_t)value;
+    data[1] = (uint8_t)(value >> 8) & 0xFF;
+    data[2] = (uint8_t)(value >> 16) & 0xFF;
+    data[3] = (uint8_t)(value >> 24) & 0xFF;
 
     W25qxx_WriteByte(data[0], address);
     W25qxx_WriteByte(data[1], address + 1);
+    W25qxx_WriteByte(data[2], address + 2);
+    W25qxx_WriteByte(data[3], address + 3);
 
     HAL_Delay(15);
 }
@@ -35,28 +42,30 @@ void tables::set_value(table_ref table, uint16_t x, uint16_t y, int16_t value)
 TABLEDATA tables::read_all(table_ref table)
 {
 
-    TABLEDATA matrix(table.y_max, vector<int16_t>(table.x_max, 0xff));
+    TABLEDATA matrix(table.y_max, vector<int32_t>(table.x_max, 0xff));
 
     uint32_t datarow = 0;
 
     for (int16_t matrix_y = 0; matrix_y < table.y_max; matrix_y++)
     {
 
-        uint32_t address = (2 * matrix_y * table.x_max) + (W25qxx_SectorToPage(table.memory_address) * w25qxx.PageSize);
+        uint32_t address = (4 * matrix_y * table.x_max) + (W25qxx_SectorToPage(table.memory_address) * w25qxx.PageSize);
 
-        uint8_t table_row[MAX_ROW_SIZE * 2];
+        uint8_t table_row[MAX_ROW_SIZE * 4];
 
-        W25qxx_ReadBytes(table_row, address, table.x_max * 2);
+        W25qxx_ReadBytes(table_row, address, table.x_max * 4);
 
         for (int16_t matrix_x = 0; matrix_x < table.x_max; matrix_x++)
         {
 
-            volatile int16_t value = ((int16_t)table_row[datarow] << 8) + table_row[datarow + 1];
+            volatile int16_t value =
+                (table_row[datarow + 1] << 8) + (table_row[datarow + 2] << 16) + (table_row[datarow + 3] << 24) + table_row[datarow];
+
             matrix[matrix_y][matrix_x] = value;
 
             auto volatile storageValue = matrix[matrix_y][matrix_x]; // DEBUG
             BREAKPOINT
-            datarow += 2;
+            datarow += 4;
         }
         datarow = 0;
     }
@@ -66,7 +75,7 @@ TABLEDATA tables::read_all(table_ref table)
 
 void tables::update_table(TABLEDATA data, table_ref table)
 {
-    int32_t size = table.x_max * 2 * table.y_max;
+    int32_t size = table.x_max * 4 * table.y_max;
     uint8_t *buffer = (uint8_t *)malloc(size);
     dump_table(data, buffer);
 
@@ -78,9 +87,9 @@ void tables::update_table(TABLEDATA data, table_ref table)
     free(buffer);
 }
 
-int16_t tables::find_nearest_neighbor(
-    std::vector<int16_t> vec,
-    int16_t search)
+int32_t tables::find_nearest_neighbor(
+    std::vector<int32_t> vec,
+    int32_t search)
 {
 
     auto const search_result = std::upper_bound(vec.begin(), vec.end(), search);
@@ -99,7 +108,7 @@ void tables::plot_table(TABLEDATA table)
 
         for (auto table_x : table_y)
         {
-            sprintf(row, "%s [%4d]", row, table_x);
+            sprintf(row, "%s [%4ld]", row, table_x);
         }
         trace_printf("%s\n", row);
         row[0] = 0;
