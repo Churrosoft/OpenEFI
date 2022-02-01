c41885b5fa1da24764354fcfb5cd51a330696e64
FDSoftware <dfrenoux@gmail.com>
2022-01-31 23:11:53 -0300

    fix MAP::get_value

--- Src/sensors/src/map.cpp
+++ Src/sensors/src/map.cpp
@@ -8,9 +8,9 @@
 
 int32_t MAP::get_value(int32_t filt_input) {
 
-  int32_t volatile sensor_volt = convert_to_volt(filt_input);
+  int32_t sensor_volt = convert_to_volt(filt_input) * 1.534;
 
-  return (int32_t)MAP_CAL(sensor_volt) / 1000;
+  return (int32_t)MAP_CAL(sensor_volt) / 10;
 }
 
 int32_t MAP::get_calibrate_value(int32_t filt_input) {
