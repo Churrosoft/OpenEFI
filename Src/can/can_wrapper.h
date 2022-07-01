f672fdf0a53f0afa06670f3489ff369037d858f7
FDSoftware <dfrenoux@gmail.com>
2022-07-01 18:50:25 -0300

    CAN Wrapper headers

---
+++ Src/can/can_wrapper.h
@@ -0,0 +1,20 @@
+#ifndef CAN_WRAPPER_H
+#define CAN_WRAPPER_H
+#include "defines.h"
+
+namespace CAN
+{
+    void send_data_to_scanner();
+
+    void update_dashdash();
+
+    uint8_t check_crc(); // en teoriiiaa puede revisarlo por su cuenta HAL y descartar los invalidos
+
+    void on_setup();
+    void on_loop();
+    void on_message(); // similar a web_serial::command_handler
+
+} // namespace CAN
+
+
+#endif
