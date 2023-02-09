635e3bed228a9af6d4b29513145ca260b4135d05
FDSoftware <dfrenoux@gmail.com>
2023-02-09 17:50:32 -0300

    add upload script

--- upload.sh
+++ upload.sh
@@ -1,5 +1,15 @@
+vidpid_to_devs(){
+        find $(grep -l "PRODUCT=$(printf "%x/%x" "0x${1%:*}" "0x${1#*:}")" \
+                  /sys/bus/usb/devices/[0-9]*:*/uevent | sed 's,uevent$,,') \
+           /dev/null -name dev -o -name dev_id |
+        sed 's,[^/]*$,uevent,' |
+        xargs sed -n -e s,DEVNAME=,/dev/,p -e s,INTERFACE=,,p
+}
+ttyPorts=$(vidpid_to_devs 1d50:6018)
+ttyPortArr=($ttyPorts)
+
 arm-none-eabi-gdb -nx --batch \
-  -ex 'target extended-remote /dev/ttyACM0' \
+  -ex "target extended-remote ${ttyPortArr}" \
   -ex 'monitor swdp_scan' \
   -ex 'attach 1' \
   -ex 'load' \
