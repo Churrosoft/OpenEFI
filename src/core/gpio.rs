4fb6207aca7dad508f6d904c6ff2090898751104
FDSoftware <dfrenoux@gmail.com>
2023-02-10 00:31:01 -0300

    wip gpio

--- src/core/gpio.rs
+++ src/core/gpio.rs
@@ -1,31 +1,45 @@
-use stm32f4xx_hal::{
-    gpio::{self, Output, Pin, PushPull, gpiod, gpioc},
-};
+use stm32f4xx_hal::gpio::{self, gpioc, gpiod, Input, Output, PushPull};
 pub struct GpioMapping {
     // LED's / User feedback
     pub led_0: gpio::PC13<Output<PushPull>>,
     pub led_1: gpio::PC14<Output<PushPull>>,
     pub led_2: gpio::PC15<Output<PushPull>>,
+
     // Injection
-    pub iny_1: Pin<'D', 8, Output<PushPull>>,
-    pub iny_2: Pin<'D', 9, Output<PushPull>>,
-    pub iny_3: Pin<'D', 10, Output<PushPull>>,
-    pub iny_4: Pin<'D', 11, Output<PushPull>>,
+    pub iny_1: gpio::PD8<Output<PushPull>>,
+    pub iny_2: gpio::PD9<Output<PushPull>>,
+    pub iny_3: gpio::PD10<Output<PushPull>>,
+    pub iny_4: gpio::PD11<Output<PushPull>>,
     // Ignition
-    pub ecn_1: Pin<'D', 12, Output<PushPull>>,
-    pub ecn_2: Pin<'D', 13, Output<PushPull>>,
-    pub ecn_3: Pin<'D', 14, Output<PushPull>>,
-    pub ecn_4: Pin<'D', 15, Output<PushPull>>,
+    pub ecn_1: gpio::PD12<Output<PushPull>>,
+    pub ecn_2: gpio::PD13<Output<PushPull>>,
+    pub ecn_3: gpio::PD14<Output<PushPull>>,
+    pub ecn_4: gpio::PD15<Output<PushPull>>,
     // PMIC
     // CKP/CMP
 
     // RELAY's
+    pub relay_iny: gpio::PE2<Output<PushPull>>,
+    pub relay_gnc: gpio::PE3<Output<PushPull>>,
+    pub relay_ac: gpio::PE4<Output<PushPull>>,
+
+    // Step-Step motor TODO
+    pub mtr_step: gpio::PE4<Output<PushPull>>,
+    pub mtr_dir: gpio::PE4<Output<PushPull>>,
+    pub mtr_fault: gpio::PE4<Output<PushPull>>,
+    pub mtr_enable: gpio::PE4<Output<PushPull>>,
 
     // AUX I/O
+    pub aux_in_1: gpio::PA0<Input>,
+    pub aux_in_2: gpio::PA1<Input>,
+    pub aux_in_3: gpio::PA3<Input>,
+
+    pub aux_out_1: gpio::PC4<Output<PushPull>>,
+    pub aux_out_2: gpio::PC5<Output<PushPull>>,
+    pub aux_out_3: gpio::PB0<Output<PushPull>>,
 }
 
 pub fn init_gpio(gpioc: gpioc::Parts, gpiod: gpiod::Parts) -> GpioMapping {
-
     let mut gpio = GpioMapping {
         // LED's / User feedback
         led_0: gpioc.pc13.into_push_pull_output(),
