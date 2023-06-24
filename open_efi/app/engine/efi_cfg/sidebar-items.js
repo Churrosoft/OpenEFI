1726ad4c61aeef6dad36164e4d1d9891ae67dc4f
GitHub CI <github@github-ci.org>
2023-06-24 20:40:35 +0000

    Deploy code docs to GitHub Pages build: 5366134432
    Commit: 9899e8feeed3fcbbbeae15d8e7bb9ecaff7cf8f6

--- open_efi/app/engine/efi_cfg/sidebar-items.js
+++ open_efi/app/engine/efi_cfg/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["FIELDS"],"enum":["__Field"],"fn":["get_default_efi_cfg"],"struct":["Engine","EngineConfig","InjectionConfig","InjectorConfig","__FieldVisitor","__Visitor"]};
+window.SIDEBAR_ITEMS = {"fn":["get_default_efi_cfg"],"struct":["Engine","EngineConfig","InjectionConfig","InjectorConfig"]};
